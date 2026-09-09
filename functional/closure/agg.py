09267f0fe480a3fd40192767d3af4cd9a324649f
nahom <nahommekuria19@gmail.com>
2026-09-09 22:54:09 +0300

    agg

---
+++ functional/closure/agg.py
@@ -0,0 +1,23 @@
+# SQLAlchemy Imports
+from sqlalchemy import func
+
+# Own Imports
+from orm.ledger import BaseLedgerORM, Userwallet
+
+
+class LedgerAggregateORM(BaseLedgerORM):
+    """ORM responsible for performing aggregate operations."""
+
+    async def total_sum(self, user_id: int):
+        """This method aggregates the amount of a user wallet."""
+
+        return (
+            self.orm.query(Userwallet)
+            .join(Userwallet.owner)
+            .filter(Userwallet.user == user_id)
+            .with_entities(func.sum(Userwallet.amount))
+            .all()
+        )
+
+
+ledger_aggregate_orm = LedgerAggregateORM()
